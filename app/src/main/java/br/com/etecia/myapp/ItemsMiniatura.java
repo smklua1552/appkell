680f78db25f0c671a5276f855e7b4468e3ce85ac
professor laercio <lndsilva@hotmail.com>
2025-08-22 21:45:35 -0300

    descobri a viagem no tempo

--- app/src/main/java/br/com/etecia/myapp/ItemsMiniatura.java
+++ app/src/main/java/br/com/etecia/myapp/ItemsMiniatura.java
@@ -8,11 +8,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class ItemsMiniatura extends AppCompatActivity {
     MaterialToolbar toolbar;
+    ViewPager2 idViewPagerItensMiniatura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +30,7 @@ public class ItemsMiniatura extends AppCompatActivity {
         toolbar.setNavigationOnClickListener((ev) -> {
             startActivity(new Intent(getApplicationContext(), MenuPrincipalActivity.class));
         });
+
+        idViewPagerItensMiniatura = findViewById(R.id.idViewPagerItensMiniatura);
     }
 }
