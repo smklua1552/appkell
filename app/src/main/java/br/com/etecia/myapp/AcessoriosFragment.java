88a01cd50c179cb161a7422f3dc74e5869381f9a
smklua1552 <samuel1552davi@gmail.com>
2025-08-22 21:29:37 -0300

    R1ECEBAAAAAAAAAAAAAAAAAA

--- app/src/main/java/br/com/etecia/myapp/AcessoriosFragment.java
+++ app/src/main/java/br/com/etecia/myapp/AcessoriosFragment.java
@@ -12,7 +12,10 @@ public class AcessoriosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_acessorios, container, false);
+        View view = inflater.inflate(R.layout.fragment_acessorios, container, false);
+
+        // voceeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee codificarrrrrrrrrrrrrrrrrrrrrrrrrrrr a partir daquiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+
+        return view;
     }
 }
--- app/src/main/java/br/com/etecia/myapp/LojaFragment.java
+++ app/src/main/java/br/com/etecia/myapp/LojaFragment.java
@@ -12,7 +12,8 @@ public class LojaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_loja, container, false);
+        View view = inflater.inflate(R.layout.fragment_loja, container, false);
+
+        return view;
     }
 }
